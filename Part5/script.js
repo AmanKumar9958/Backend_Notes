1b265c99b124cfbc81c562c8bb51e7d27716e871
Aman Kumar <amancollege04@gmail.com>
2025-05-08 18:11:27 +0530

    Lecture 05: Learned use of Cookies and Sessions

--- Part5/script.js
+++ Part5/script.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+// this line is used to make our data readable from the frontend
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', function(req, res){
     res.send("I am Express JS")
 })
