2de506d3c2763b628d49bb86a4703b7a9bbcef61
Aman Kumar <amancollege04@gmail.com>
2025-05-12 16:49:35 +0530

    Lecture 06: Completed dynamic routing

--- Part6/script.js
+++ Part6/script.js
@@ -12,13 +12,22 @@ app.set('view engine', 'ejs');
 // setting up public static files..
 app.use(express.static(path.join(__dirname, 'public')))
 
-// rendering ejs
+// rendering ejs..
 app.get('/', (req, res) => {
     res.render("index")
 })
 
 app.get('/about', (req, res) => {
     res.render("about")
+})
+
+// dynamic routing..
+app.get('/profile/:username', (req, res) => {
+    res.send(`Welcome, ${req.params.username}`)
+})
+
+app.get('/ring/:metal/:carat', (req, res) => {
+    res.send(`This is ${req.params.carat} carat ${req.params.metal} ring`)
 })
 
 app.listen(3000);
